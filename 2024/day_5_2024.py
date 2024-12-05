7dafeb672321a45fb6833af587783ae3dc6a94a6
RandoPerso <86388449+RandoPerso@users.noreply.github.com>
2024-12-05 01:24:24 -0500

    Add 2024 day 5 solution

---
+++ 2024/day_5_2024.py
@@ -0,0 +1,51 @@
+import aoctools
+from copy import deepcopy
+import re
+from functools import cmp_to_key, cache
+
+tools = aoctools.aoc_tools()
+
+tools.start_clock()
+lines = tools.get_array_input("2024/inputs/day_5_2024.txt")
+
+total = 0
+rules = []
+wrong = []
+started = False
+for line in lines:
+    if started:
+        for rule in rules:
+            if re.findall(rule[1] + ".*" + rule[0], line):
+                wrong.append(line)
+                break
+        else:
+            update = line.split(",")
+            total += int(update[int((len(update) - 1)/2) ])
+    else:
+        if line == "":
+            started = True
+        else:
+            rules.append(line.split("|"))
+
+print(total)
+
+tools.stop_clock()
+
+total = 0
+for line in wrong:
+    update = line.split(",")
+    fixed = False
+    while not fixed:
+        fixed = True
+        for rule in rules:
+            if rule[0] in update and rule[1] in update:
+                a, b = (update.index(rule[0]), update.index(rule[1]))
+                if a > b:
+                    fixed = False
+                    update[a] = rule[1]
+                    update[b] = rule[0]
+    total += int(update[int((len(update) - 1)/2) ])
+
+print(total)
+
+tools.stop_clock()
